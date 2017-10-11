ff83d3aa85cc9de7957ddfe5807af36c8a1c84ad
NorseUA <norseua@gmail.com>
2017-10-11 11:32:31 +0300

    add ci

--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-export const weekDayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sut', 'Sun'];
